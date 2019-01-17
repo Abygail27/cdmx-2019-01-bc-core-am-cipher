75a78fe7769549725b166a04c60ffbd3f47d9ee0
Abygail27 <abygaailrrocha@gmail.com>
2019-01-17 16:16:37 -0600

    ultima versión funcional.

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,32 @@
 window.cipher = {
-  // ... 
-};
+
+    encode: (offset, text) => {
+        let msj = ""
+        for (let i = 0; i < text.length; i++) {
+            const codigo_ascii = text.charCodeAt(i);
+            const formula_ascii = (codigo_ascii - 65 + offset) % 26 + 65;
+            const codigo_cifrado = String.fromCharCode(formula_ascii);
+            msj += codigo_cifrado;
+
+
+        }
+
+        //  console.log(offset,msj);
+        return msj;
+    },
+    decode: (offset, text) => {
+        let msj = ""
+        for (let i = 0; i < text.length; i++) {
+            const codigo_ascii = text.charCodeAt(i);
+            const formula_ascii = (codigo_ascii + 65 - offset) % 26 + 65;
+            const codigo_descifrado = String.fromCharCode(formula_ascii);
+            msj += codigo_descifrado;
+
+
+        }
+
+        //  console.log(offset,msj);
+        return msj;
+    }
+
+};        
---
+++ src/index.js
@@ -0,0 +1,31 @@
+    
+    // crear variables globales para llamarlas en funciones
+
+    const mensaje_principal = document.getElementById ('text');
+    const offset = document.getElementById ('offset');
+    const en_code = document.getElementById ('encode');
+    const de_code = document.getElementById ('decode');
+    const clean = document.getElementById ('three');
+    //const print_text = document.getElementById ('print');
+
+        
+    //crear botones con funcionalidad para encode, decode
+    en_code.addEventListener ('click', ( )  => {
+    let des_pla = parseInt(offset.value);
+    let msj = mensaje_principal.value.toUpperCase();
+    document.getElementById('print_text').textContent = window.cipher.encode (des_pla,msj);
+    //console.log(en_code);
+    }); 
+
+    de_code.addEventListener ('click' , ( ) => {
+    let des_pla = parseInt (offset.value);
+    let msj = mensaje_principal.value.toUpperCase();
+    document.getElementById ('print_text').textContent = window.cipher.decode (des_pla,msj);
+    //console.log(de_code); 
+    });
+
+    //funcion para refrescar la pagina 
+    const refrescar = () => {
+        location.reload (true);
+    };
+    clean.addEventListener ('click', refrescar); 

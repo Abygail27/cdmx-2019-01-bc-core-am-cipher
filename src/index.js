a33844dd72ab547df23347c109bd73203fbeee2a
Abygail27 <abygaailrrocha@gmail.com>
2019-01-23 20:59:44 -0600

    js done

--- src/index.js
+++ src/index.js
@@ -6,22 +6,21 @@
     const en_code = document.getElementById ('encode');
     const de_code = document.getElementById ('decode');
     const clean = document.getElementById ('three');
-    //const print_text = document.getElementById ('print');
-
+    
         
     //crear botones con funcionalidad para encode, decode
     en_code.addEventListener ('click', ( )  => {
     let des_pla = parseInt(offset.value);
     let msj = mensaje_principal.value;
     document.getElementById('print_text').textContent = window.cipher.encode (des_pla,msj);
-    //console.log(en_code);
+
     }); 
 
     de_code.addEventListener ('click' , ( ) => {
     let des_pla = parseInt (offset.value);
     let msj = mensaje_principal.value;
     document.getElementById ('print_text').textContent = window.cipher.decode (des_pla,msj);
-    //console.log(de_code); 
+
     });
 
     //funcion para refrescar la pagina 
@@ -33,8 +32,7 @@
     //funcion pagina instrucciones
     const caja_principal = document.getElementById ('caja_principal');
     const caja_dos =document.getElementById ('instrucciones');
-    //const help = document.getElementById ('help');
-    //const menu = document.getElementById ('menu');
+    
    
     document.getElementById ('help').addEventListener ('click',( ) => {
         caja_principal.style.display = 'none';
